aa1608ec31374a6f92fd68eeb59b8cc7cc336033
Ronan <roleary81@gmail.com>
2020-12-30 21:03:05 +0100

    toggle mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,19 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react';
 
 let initialState = {
-    logo: 'LOGO'
+    logo: 'LOGO',
+    showNav: false
 }
 
 
 const Nav = () => {
     const [val, setVal] = useState(initialState.logo); 
+    const [showNav, setShowNav] = useState(initialState.showNav);
     const toggleNav = (e) => {
-        setVal('DICKBALLS');
+        setShowNav(!showNav);
+        console.log(showNav)
     }
+
 
     return(
         <nav className="bg-gray-800">
@@ -18,7 +22,7 @@ const Nav = () => {
             <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 
-                <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+                <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false" onClick={(e) => toggleNav(e)}>
                 <span className="sr-only">Open main menu</span>
                
                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -31,7 +35,7 @@ const Nav = () => {
                 </button>
             </div>
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <a className="flex-shrink-0 flex items-center" onClick={(e) => toggleNav(e)}>
+                <a className="flex-shrink-0 flex items-center">
                     [LOGO]
                 </a>
                 <div className="hidden sm:block sm:ml-6">
@@ -83,12 +87,23 @@ const Nav = () => {
             </div>
         </div>
 
-        <div className="hidden sm:hidden">
+        <div className={!showNav ? "hidden sm:hidden" : ""}>
             <div className="px-2 pt-2 pb-3 space-y-1">
-            <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                <Link href='/'>
+                    <a className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+                </Link>
+                <Link href='/sample-page'>
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Flerp</a>
+                </Link>
+                <Link href='/blog'>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Blog</a>
+                </Link>
+                <Link href='/products'>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Products</a>
+                </Link>
+                <Link href='/documents'>
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Documents</a>
+                </Link>
             </div>
         </div>
     </nav>
